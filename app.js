c58cdc9f99b2b21ccc11b0ff5e8cf3ed107b150b
Kartik47 <kumthekarkartik@gmail.com>
2025-05-18 12:33:39 +0530

    CRUD in postman

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 require('dotenv').config();
 
-// const productRoutes = require("../ProductCrud/routes/productRoutes")
-// const categoryRoutes = require('../ProductCrud/routes/categoryRoutes')
 const productRoutes = require("../ProductCrud/routes/productRoutes")
+const brandRoutes = require("./routes/brandRoute")
+const categoryRoutes = require('./routes/categoryRoutes')
 
 const port = process.env.PORT
 
@@ -12,9 +12,13 @@ app.use(express.json());
 
 app.use('/api/product',productRoutes)
 
+app.use ('/api/category',categoryRoutes)
+
+app.use('/api/brand', brandRoutes);
+
 
 
 
 app.listen(port,()=> console.log(`Example app listening on port ${port}!`))
 
-// http://localhost:3000/api/products
+// http://localhost:7000/api/products
--- controller/productsController.js
+++ controller/productsController.js
@@ -1,28 +1,5 @@
-const products = [
-  {
-    id: 1,
-    name: "iPhone15pro",
-    category: "Electronics",
-    price: 120000,
-    instock: true,
-  },
-  {
-    id: 2,
-    name: "iPhone16pro",
-    category: "Electronics",
-    price: 120200,
-    instock: true,
-  },
-  { id: 3, name: "Rice", category: "Grocery", price: 120200, instock: true },
-  {
-    id: 4,
-    name: "Laptop",
-    category: "Electronics",
-    price: 120200,
-    instock: true,
-  },
-  { id: 5, name: "Cargo", category: "Clothes", price: 120200, instock: true },
-];
+const products = require("../data/product")
+const categories = require("../data/category")
 
 function addProduct(req, res) {
   console.log("----", req.body, "-----");
